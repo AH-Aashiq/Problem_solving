dd71f749635ab1427d42364535b4dc99dd84da54
AH-Aashiq <arifulhaq215@gmail.com>
2021-10-02 15:43:43 +0600

    URI problem

--- 1015.py
+++ 1015.py
@@ -1,1 +1,9 @@
 #Distance Between Two Points
+import  math
+
+x1,y1=list(map(float,input().split()))                              
+x2,y2=list(map(float,input().split()))
+distance = math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
+
+print(f'{distance:0.4f}')
+
