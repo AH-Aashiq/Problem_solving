98e2847d06f97c5f35fcccfc722fc043d2a42833
AH-Aashiq <arifulhaq215@gmail.com>
2021-10-04 12:45:46 +0600

    URI Solve

--- 1021.py
+++ 1021.py
@@ -1,1 +1,15 @@
 #Banknotes and Coins
+a = float(input())
+print('NOTAS:')
+
+for i in [100,50,20,10,5,2]:
+     print(f'{int(a/i)} nota(s) de R$ {i}.00')
+     a = float(f'{a%i:.2f}')
+# print (a)
+
+print('MOEDAS:')
+
+for i in [1.00,0.50,0.25,0.10,0.05,0.01]:
+     print(f'{int(a/i)} moeda(s) de R$ {i:.2f}')
+     a = float(f'{a % i:.2f}')
+# print (a)
