7379eed01428bf5fbb1da826d78fbfd4620f04fc
AH-Aashiq <arifulhaq215@gmail.com>
2021-10-29 23:08:02 +0600

    interview

--- interview.py
+++ interview.py
@@ -122,3 +122,15 @@
 #      return alphaset <= set(str1.lower())
 
 # print(ispangram("The quick brown fox jumps over the lazy dog"))
+
+
+#Find the prime number 
+lower = int(input("Enter the lower value:"))
+upper = int(input("Enter the upper value:"))
+for number in range(lower,upper+1):
+    if number>1:
+        for i in range(2,number):
+            if (number%i)==0:
+                break
+        else:
+            print(number)
